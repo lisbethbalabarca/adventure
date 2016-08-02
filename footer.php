a062bf4b6c49de0260a913b888690ecdc7d2fca3
lisbethbalabarca <lisbethbalabarca@gmail.com>
2016-08-02 14:49:04 -0500

    cambios

---
+++ footer.php
@@ -0,0 +1,169 @@
+<!--Footer Widget Area Start-->
+        <div class="footer-widget-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 col-md-3 col-sm-4">
+                        <div class="single-footer-widget contact-text-info">
+                            <h4>Contact Us</h4>
+                            <div class="footer-widget-list">
+                                <ul>
+                                    <li class="icon send">19 Charlotte Street, Toronto Ontario, M5V 2H5</li>
+                                    <li class="icon envelope">admin@power-boosts.com</li>
+                                    <li class="icon phone">+ 001 666 8989 55</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-2 hidden-sm">
+                        <div class="single-footer-widget">
+                            <h4>About Us</h4>
+                            <div class="footer-widget-list">
+                                <ul class="widget-lists">
+                                    <li><a href="#">Why Us</a></li>
+                                    <li><a href="#">Our trips</a></li>
+                                    <li><a href="#">Responsible Business</a></li>
+                                    <li><a href="#">Our History</a></li>
+                                    <li><a href="#">Our Core Values</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 hidden-md hidden-sm">
+                        <div class="single-footer-widget">
+                            <h4>Design Themes</h4>
+                            <div class="footer-widget-list">
+                                <ul class="widget-lists">
+                                    <li><a href="#">Africa</a></li>
+                                    <li><a href="#">Asia</a></li>
+                                    <li><a href="#">Central America</a></li>
+                                    <li><a href="#">Europe</a></li>
+                                    <li><a href="#">Middle East</a></li>
+                                    <li><a href="#">North America</a></li>
+                                    <li><a href="#">Oceania</a></li>
+                                    <li><a href="#">South America</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-3">
+                        <div class="single-footer-widget">
+                            <h4>Destinations</h4>
+                            <div class="footer-widget-list">
+                                <ul class="widget-lists">
+                                    <li><a href="#">Adventure</a></li>
+                                    <li><a href="#">Hiking and Camping</a></li>
+                                    <li><a href="#">Walking and Trekking</a></li>
+                                    <li><a href="#">Safari Trip</a></li>
+                                    <li><a href="#">Polar</a></li>
+                                    <li><a href="#">Sailing</a></li>
+                                    <li><a href="#">Climbing</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-4 col-sm-5">
+                        <div class="single-footer-widget">
+                            <h4>Latest Tweets</h4>
+                            <div class="footer-widget-list twitter-news">
+                                <ul>
+                                    <li><a href="#">@Power-Boosts</a> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremq-ue laudantium. <a href="#">Power-Boosts.com/building-a-website</a></li>
+                                    <li><a href="#">@Power-Boosts</a> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremq-ue laudantium. <a href="#">Power-Boosts.com/building-a-website</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 col-sm-6">
+                        <div class="footer-link">
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+                            <a href="#"><i class="fa fa-twitter"></i></a>
+                            <a href="#"><i class="fa fa-google-plus"></i></a>
+                            <a href="#"><i class="fa fa-linkedin"></i></a>
+                            <a href="#"><i class="fa fa-rss"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-sm-6">
+                        <div class="payment-image">
+                            <img src="img/payment.png" alt="">
+                        </div>    
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--End of Footer Widget Area-->
+        <!--Footer Area Start-->
+        <div class="footer-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 col-md-5 col-sm-12">
+                        <span>Copyright © 2016 <a href="#">Power-Boosts</a>. All rights reserved.</span>
+                    </div>
+                    <div class="col-lg-8 col-md-7 col-sm-12">
+                        <nav>
+                            <ul id="footer-menu">
+                                <li><a href="index.html">Home</a></li>
+                                <li><a href="shop-grid-no-sidebar.html">Destinations</a></li>
+                                <li><a href="shop-grid-no-sidebar.html">Travel Styles</a></li>
+                                <li><a href="shop-grid-no-sidebar.html">Specials</a></li>
+                                <li><a href="blog-1.html">Blog</a></li>
+                                <li><a href="contact.html">Contact Us</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--End of Footer Area-->
+        
+        
+		<!-- jquery
+		============================================ -->		
+        <script src="js/vendor/jquery-1.12.3.min.js"></script>
+        
+		<!-- bootstrap JS
+		============================================ -->		
+        <script src="js/bootstrap.min.js"></script>
+        
+        <!-- nivo slider js
+		============================================ -->       
+		<script src="lib/nivo-slider/js/jquery.nivo.slider.js" type="text/javascript"></script>
+		<script src="lib/nivo-slider/home.js" type="text/javascript"></script>
+		
+		<!-- wow JS
+		============================================ -->		
+        <script src="js/wow.min.js"></script>
+        
+		<!-- meanmenu JS
+		============================================ -->		
+        <script src="js/jquery.meanmenu.js"></script>
+        
+		<!-- owl.carousel JS
+		============================================ -->		
+        <script src="js/owl.carousel.min.js"></script>
+        
+		<!-- price-slider JS
+		============================================ -->		
+        <script src="js/jquery-price-slider.js"></script>
+        
+		<!-- scrollUp JS
+		============================================ -->		
+        <script src="js/jquery.scrollUp.min.js"></script>
+        
+		<!-- Waypoints JS
+		============================================ -->		
+        <script src="js/waypoints.min.js"></script>
+        
+		<!-- Counter Up JS
+		============================================ -->		
+        <script src="js/jquery.counterup.min.js"></script>
+        
+		<!-- plugins JS
+		============================================ -->		
+        <script src="js/plugins.js"></script>
+        
+		<!-- main JS
+		============================================ -->		
+        <script src="js/main.js"></script>
+    </body>
+</html>
---
+++ header.php
@@ -0,0 +1,287 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Home One || Adventures</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+		<!-- favicon
+		============================================ -->		
+        <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
+		
+		<!-- Google Fonts
+		============================================ -->		
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Raleway:400,300,500,600,700,800' rel='stylesheet' type='text/css'>
+	   
+		<!-- Bootstrap CSS
+		============================================ -->		
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        
+		<!-- Fontawsome CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        
+		<!-- owl.carousel CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/owl.carousel.css">
+        
+		<!-- jquery-ui CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/jquery-ui.css">
+        
+		<!-- meanmenu CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/meanmenu.min.css">
+        
+		<!-- animate CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/animate.css">
+        
+        <!-- nivo slider CSS
+		============================================ -->
+		<link rel="stylesheet" href="lib/nivo-slider/css/nivo-slider.css" type="text/css" />
+		<link rel="stylesheet" href="lib/nivo-slider/css/preview.css" type="text/css" media="screen" />
+        
+		<!-- style CSS
+		============================================ -->
+        <link rel="stylesheet" href="style.css">
+        
+		<!-- responsive CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/responsive.css">
+        
+		<!-- modernizr JS
+		============================================ -->		
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        
+        <!--Header Area Start-->
+        <header>
+            <div class="header-top">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4 col-sm-6 hidden-xs">
+                            <div class="currency-language">
+                                <div class="currency-menu">
+                                    <ul>
+                                        <li><a href="#">USD <i class="fa fa-angle-down"></i></a>
+                                            <ul class="currency-dropdown">
+                                                <li><a href="#">EUR</a></li>
+                                                <li><a href="#">USD</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="language-menu">
+                                    <ul>
+                                        <li><a href="#">English <i class="fa fa-angle-down"></i></a>
+                                            <ul class="language-dropdown">
+                                                <li><a href="#">French</a></li>
+                                                <li><a href="#">English</a></li>
+                                                <li><a href="#">German</a></li>
+                                                <li><a href="#">Arabic</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div> 
+                        <div class="col-md-8 col-sm-6 col-xs-12">
+                            <div class="header-top-right">
+                                <div class="login">
+                                    <a href="#"><i class="fa fa-pencil-square-o"></i>Register</a>
+                                </div>
+                                <div class="account">
+                                    <a href="signin.html"><i class="fa fa-lock"></i>sign In</a>
+                                </div>
+                                <ul class="header-r-cart">
+                                    <li><a href="#" class="cart"><i class="fa fa-shopping-cart"></i>(2)</a>
+                                        <div class="mini-cart-content">
+                                            <div class="cart-products-list">
+                                                <div class="cart-products">
+                                                    <div class="cart-image">
+                                                        <a href="#"><img alt="" src="img/cart/1.jpg"></a>
+                                                    </div>
+                                                    <div class="cart-product-info">
+                                                        <a class="product-name" href="#">alteration</a>
+                                                        <span class="quantity">X 1 -</span>
+                                                        <span class="p-price">$23.87</span>
+                                                        <a class="remove-product"><i class="fa fa-times-circle"></i></a>
+                                                    </div>
+                                                </div>
+                                                <div class="cart-products">
+                                                    <div class="cart-image">
+                                                        <a href="#"><img alt="" src="img/cart/2.jpg"></a>
+                                                    </div>
+                                                    <div class="cart-product-info">
+                                                        <a class="product-name" href="#">alteration</a>
+                                                        <span class="quantity">X 1 -</span>
+                                                        <span class="p-price">$332.28</span>
+                                                        <a class="remove-product"><i class="fa fa-times-circle"></i></a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="cart-price-list">
+                                                <p class="price-amount">Sub-Total : <span>$293.73</span> </p>
+                                                <p class="price-amount">Total : <span>$356.15</span> </p>
+                                                <div class="cart-buttons">
+                                                    <a href="#"><i class="fa fa-shopping-cart"></i> View Cart</a>
+                                                    <a href="#"><i class="fa fa-sign-in"></i> Checkout</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>    
+                        </div>
+                    </div>
+                </div>
+            </div>  
+            <!--Logo Mainmenu Start-->
+            <div class="header-logo-menu">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="logo-menu-bg">
+                                <div class="row">
+                                    <div class="col-md-4 col-sm-12">
+                                        <div class="logo">
+                                            <a href="index.html"><img src="img/logo/logo.png" alt="ADVENTURES"></a>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-8 hidden-sm hidden-xs">
+                                        <div class="mainmenu">
+                                            <nav>
+                                                <ul id="nav">
+                                                    <li class="drop-down"><a href="index.html">HOME</a>
+                                                        <ul class="sub-menu">
+                                                            <li><a href="#" class="mega-title">Homepages</a></li>
+                                                            <li><a href="index-2.html">Homepage Version 2</a></li>
+                                                            <li><a href="index-3.html">Homepage Version 3</a></li>
+                                                        </ul>
+                                                    </li>
+                                                    <li><a href="shop-grid-no-sidebar.html">Destination</a>
+                                                        <div class="megamenu">
+                                                            <div class="megamenu-list clearfix">
+                                                                <span class="border-hover">
+                                                                    <a href="#" class="mega-image">
+                                                                        <img src="img/mega-b-4.jpg" alt="">
+                                                                    </a>
+                                                                </span>
+                                                                <span>											
+                                                                    <a href="#" class="mega-title">Edgeworks Climbing</a>
+                                                                    <a href="#">Indoor climbing</a>
+                                                                    <a href="#">Rock climbing</a>
+                                                                    <a href="#">Climbing competition</a>
+                                                                    <a href="#">Top rope climbing</a>
+                                                                    <a href="#">Traditional climbing</a>
+                                                                    <a href="#">Free solo climbing</a>
+                                                                </span>
+                                                                <span>											
+                                                                    <a href="#" class="mega-title">Academic Mountain</a>
+                                                                    <a href="#">Fold and thrust belt</a>
+                                                                    <a href="#">Fault-block mountain</a>
+                                                                    <a href="#">Mountain ranges</a>
+                                                                    <a href="#">Sedimentary</a>
+                                                                    <a href="#">Subaqueous volcano</a>
+                                                                    <a href="#">Complex volcano</a>
+                                                                </span>
+                                                                <span>											
+                                                                    <a href="#" class="mega-title">Adventure Camping</a>
+                                                                    <a href="#">Bicycle Camping</a>
+                                                                    <a href="#">Canoe Camping</a>
+                                                                    <a href="#">Reenact camping</a>
+                                                                    <a href="#">Winter camping</a>
+                                                                    <a href="#">Dry camping</a>
+                                                                    <a href="#">Backpacking</a>
+                                                                </span>
+                                                            </div>
+                                                        </div>
+                                                    </li>
+                                                    <li><a href="shop-grid-no-sidebar.html">Travel</a></li>
+                                                    <li class="drop-down"><a href="index.html">Pages</a>
+                                                        <ul class="sub-menu">
+                                                            <li><a href="#" class="mega-title">Other Pages</a></li>
+                                                            <li><a href="about.html">About Us</a></li>
+                                                            <li><a href="shop-grid-no-sidebar.html">Shop No Sidebar</a></li>
+                                                            <li><a href="shop-grid-with-sidebar.html">Shop with Sidebar</a></li>
+                                                            <li><a href="shop-list.html">Shop List</a></li>
+                                                            <li><a href="product-details.html">Product Details 1</a></li>
+                                                            <li><a href="product-details-2.html">Product Details 2</a></li>
+                                                            <li><a href="signin.html">Sign In</a></li>
+                                                            <li><a href="404.html">404 Error</a></li>
+                                                        </ul>
+                                                    </li>
+                                                    <li class="drop-down"><a href="blog-1.html">BLOG</a>
+                                                        <ul class="sub-menu">
+                                                            <li><a href="#" class="mega-title">Blog Pages</a></li>
+                                                            <li><a href="blog-2.html">Blog Page 2</a></li>
+                                                            <li><a href="blog-details.html">Blog Details Page</a></li>
+                                                        </ul>
+                                                    </li>
+                                                    <li><a href="contact.html">Contact Us</a></li>
+                                                </ul>
+                                            </nav>
+                                        </div> 
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>  
+            <!--End of Logo Mainmenu-->
+            <!-- Mobile Menu Area start -->
+            <div class="mobile-menu-area">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-sm-12">
+                            <div class="mobile-menu">
+                                <nav id="dropdown">
+                                    <ul>
+                                        <li><a href="index.html">HOME</a>
+                                            <ul>
+                                                <li><a href="index-2.html">Home 2</a></li>
+                                                <li><a href="index-3.html">Home 3</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="#">Shop</a>
+                                            <ul>
+                                                <li><a href="shop-grid-no-sidebar.html">Shop No Sidebar</a></li>
+                                                <li><a href="shop-grid-with-sidebar.html">Shop with Sidebar</a></li>
+                                                <li><a href="shop-list.html">Shop List</a></li>
+                                                <li><a href="product-details.html">Product Details 1</a></li>
+                                                <li><a href="product-details-2.html">Product Details 2</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="blog-1.html">Blog</a>
+                                            <ul>
+                                                <li><a href="blog-2.html">Blog Page 2</a></li>
+                                                <li><a href="blog-details.html">Blog Details</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="about.html">ABOUT</a>
+                                        <li><a href="#">PAGES</a>
+                                            <ul>
+                                                <li><a href="signin.html">Sign in</a></li>
+                                                <li><a href="404.html">404 error</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="contact.html">CONTACT</a></li>
+                                    </ul>
+                                </nav>
+                            </div>					
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Mobile Menu Area end -->
+        </header>
+        <!--End of Header Area-->
