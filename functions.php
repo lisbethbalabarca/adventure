1c85d7b7012bc1e19e99ab6f9df9fc5e79d6f2ad
lisbethbalabarca <lisbethbalabarca@gmail.com>
2016-08-02 17:07:20 -0500

    se agrego post_type

--- functions.php
+++ functions.php
@@ -1,5 +1,13 @@
 <?php 
 	function config_site(){
+		register_post_type(
+			'noticias', array(
+				'labels' => array('name' => 'Noticias', 'singular_name' => 'Noticia' ),
+				'public' => true,
+				'rewrite' => array('slug' => 'noticia'),
+				'supports' => array('title' , 'editor' , 'author' , 'thumbnail' )
+			)
+		);
 		//funcion para crear tamaño de imagenes
 		add_image_size('banner', 1600, 987, true);
 		//registramos los menus
@@ -7,14 +15,15 @@
 			'Principal' => __('Principal')
 		));
 	}
+	
 	add_theme_support('post-thumbnails');
 	add_action('init', 'config_site');
 
 	function active_menu($classes) {
-	if(in_array('current-menu-item', $classes)) {
-		$classes[] = 'active';
-	}
-	return $classes;
+		if(in_array('current-menu-item', $classes)) {
+			$classes[] = 'active';
+		}
+		return $classes;
 	}
 	add_filter('nav_menu_css_class' , 'active_menu');
 ?>
