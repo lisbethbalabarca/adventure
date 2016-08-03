b33ccfd42d512c55ddb50c4be6f458f2cce0ec14
lisbethbalabarca <lisbethbalabarca@gmail.com>
2016-08-03 15:53:01 -0500

    sin sufrimiento no hay gloria

---
+++ archive.php
@@ -0,0 +1,273 @@
+<?php get_header(); ?>
+	 <!--End of Header Area-->
+        <!--Banner Area Start-->
+        <div class="banner-area blog-one">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="section-title text-center">
+                            <div class="title-border">
+                                <h1>Adventure <span>Blog</span></h1>
+                            </div>    
+                            <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dolor turpis, pulvinar varius dui<br> id, convallis iaculis eros. Praesent porta lacinia elementum.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        <ul class="breadcrumb">
+                            <li><a href="index.html">Home</a></li>
+                            <li>Blog</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--End of Banner Area-->
+        <!--Blog Post Area Start-->
+        <div class="blog-post-area section-padding">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="sidebar-widget">
+                            <div class="single-sidebar-widget">
+                                <h4>Search <span>Blog</span></h4>
+                                <form id="text-search" action="#">
+                                    <input type="text" placeholder="Search Here .....">
+                                    <button class="submit"><i class="fa fa-search"></i></button>
+                                </form>
+                            </div>
+                            <div class="clearfix"></div>
+                            <div class="single-sidebar-widget country-select">
+                                <h4>Select <span>Categories</span></h4>
+                                <ul class="widget-categories">
+                                    <li><a href="#">Hiking <span>(10)</span></a></li>
+                                    <li><a href="#">Camping <span>(175)</span></a></li>
+                                    <li><a href="#">Trekking <span>(25)</span></a></li>
+                                    <li><a href="#">Safari <span>(18)</span></a></li>
+                                    <li><a href="#">Polar <span>(247)</span></a></li>
+                                    <li><a href="#">Mountain <span>(95)</span></a></li>
+                                    <li><a href="#">Biking <span>(39)</span></a></li>
+                                    <li><a href="#">Climbing <span>(69)</span></a></li>
+                                    <li class="no-margin"><a href="#" class="no-margin">Sailing <span>(27)</span></a></li>  
+                                </ul>
+                            </div>
+                            <div class="single-sidebar-widget">
+                                <h4>Recent <span>Posts</span></h4>
+                                <div class="single-widget-posts">
+                                    <div class="post-img">
+                                        <a href="#"><img src="img/blog/3.jpg" alt=""></a>
+                                    </div>
+                                    <div class="posts-text">
+                                        <h4><a href="#">Himalaia Trip | Europe</a></h4>
+                                        <p><i class="fa fa-clock-o"></i> May 27, 2015</p>
+                                    </div>
+                                </div>
+                                <div class="single-widget-posts">
+                                    <div class="post-img">
+                                        <a href="#"><img src="img/blog/4.jpg" alt=""></a>
+                                    </div>
+                                    <div class="posts-text">
+                                        <h4><a href="#">Himalaia Trip | Nepal</a></h4>
+                                        <p><i class="fa fa-clock-o"></i> Aug 09, 2016</p>
+                                    </div>
+                                </div>
+                                <div class="single-widget-posts no-margin">
+                                    <div class="post-img">
+                                        <a href="#"><img src="img/blog/5.jpg" alt=""></a>
+                                    </div>
+                                    <div class="posts-text">
+                                        <h4><a href="#">Himalaia Trip | China</a></h4>
+                                        <p><i class="fa fa-clock-o"></i> Jun 22, 2016</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="single-sidebar-widget">
+                                <h4>Blog <span>Archives</span></h4>
+                                <div class="blog-archive">
+                                    <select class="archive" name="archive">
+                                        <option>Select Month</option>
+                                        <option>January</option>
+                                        <option>February</option>
+                                        <option>March</option>
+                                        <option>April</option>
+                                        <option>May</option>
+                                        <option>June</option>
+                                        <option>July</option>
+                                        <option>August</option>
+                                        <option>September</option>
+                                        <option>October</option>
+                                        <option>November</option>
+                                        <option>December</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="single-sidebar-widget icon-bottom tooltip-icons">
+                                <h4>Blog <span>Tags</span></h4>
+                                <div class="widget-icon">
+                                    <span><a href="#" data-toggle="tooltip" title="Tents"><img alt="" src="img/icon/25.png"></a></span>
+                                    <span><a href="#" data-toggle="tooltip" title="Hiking"><img alt="" src="img/icon/26.png"></a></span>
+                                    <span><a href="#" data-toggle="tooltip" title="Cycling"><img alt="" src="img/icon/27.png"></a></span>
+                                    <span><a href="#" data-toggle="tooltip" title="Beach"><img alt="" src="img/icon/28.png"></a></span>
+                                    <span class="no-margin"><a href="#" data-toggle="tooltip" title="Ship Tour"><img alt="" src="img/icon/29.png"></a></span>
+                                    <span class="no-margin"><a href="#" data-toggle="tooltip" title="Boat Tour"><img alt="" src="img/icon/30.png"></a></span>
+                                    <span class="no-margin"><a href="#" data-toggle="tooltip" title="Water Games"><img alt="" src="img/icon/31.png"></a></span>
+                                    <span class="no-margin"><a href="#" data-toggle="tooltip" title="Jungle"><img alt="" src="img/icon/32.png"></a></span>
+                                </div>
+                            </div>
+                            <div class="clearfix"></div>
+                            <div class="single-sidebar-widget widget-gallery">
+                                <h4>Photo <span>Gallery</span></h4>
+                                <div class="row">
+                                    <div class="col-md-4 col-sm-2 col-xs-4">
+                                        <a href="#"><img src="img/blog/6.jpg" alt=""></a>
+                                    </div>
+                                    <div class="col-md-4 col-sm-2 col-xs-4">
+                                        <a href="#"><img src="img/blog/7.jpg" alt=""></a>
+                                    </div>
+                                    <div class="col-md-4 col-sm-2 col-xs-4">
+                                        <a href="#"><img src="img/blog/8.jpg" alt=""></a>
+                                    </div>
+                                    <div class="col-md-4 col-sm-2 col-xs-4">
+                                        <a href="#"><img src="img/blog/9.jpg" alt=""></a>
+                                    </div>
+                                    <div class="col-md-4 col-sm-2 col-xs-4">
+                                        <a href="#"><img src="img/blog/10.jpg" alt=""></a>
+                                    </div>
+                                    <div class="col-md-4 col-sm-2 col-xs-4">
+                                        <a href="#"><img src="img/blog/11.jpg" alt=""></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-9">
+                        <div class="single-blog-post">
+                            <div class="single-blog-post-img">
+                                <a href="#"><img src="img/blog/large-1.jpg" alt=""></a>
+                                <div class="date-time">
+                                    <span class="date">10</span>
+                                    <span class="month">AUG</span>
+                                </div>
+                            </div>
+                            <div class="single-blog-post-text">
+                                <h4><a href="blog-details.html">How's the weather? Best times to travel.....</a></h4>
+                                <div class="author-comments">
+                                    <span><i class="fa fa-user"></i>ALICE HINES</span>
+                                    <span><i class="fa fa-comment"></i>15 Comments</span>
+                                </div>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
+                            </div>
+                            <div class="blog-button-links">
+                                <a href="blog-details.html" class="blog-button">Learn More</a>
+                                <div class="blog-links">
+                                    <a href="#"><i class="fa fa-facebook"></i></a>
+                                    <a href="#"><i class="fa fa-twitter"></i></a>
+                                    <a href="#"><i class="fa fa-google-plus"></i></a>
+                                    <a href="#"><i class="fa fa-linkedin"></i></a>
+                                    <a href="#"><i class="fa fa-rss"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="single-blog-post">
+                            <div class="single-blog-post-img">
+                                <a href="#"><img src="img/blog/large-2.jpg" alt=""></a>
+                                <div class="date-time">
+                                    <span class="date">19</span>
+                                    <span class="month">Oct</span>
+                                </div>
+                            </div>
+                            <div class="single-blog-post-text">
+                                <h4><a href="blog-details.html">It has survived not only five centuries.....</a></h4>
+                                <div class="author-comments">
+                                    <span><i class="fa fa-user"></i>JHON RUSSEL</span>
+                                    <span><i class="fa fa-comment"></i>15 Comments</span>
+                                </div>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
+                            </div>
+                            <div class="blog-button-links">
+                                <a href="blog-details.html" class="blog-button">Learn More</a>
+                                <div class="blog-links">
+                                    <a href="#"><i class="fa fa-facebook"></i></a>
+                                    <a href="#"><i class="fa fa-twitter"></i></a>
+                                    <a href="#"><i class="fa fa-google-plus"></i></a>
+                                    <a href="#"><i class="fa fa-linkedin"></i></a>
+                                    <a href="#"><i class="fa fa-rss"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="single-blog-post">
+                            <div class="single-blog-post-img">
+                                <a href="#"><img src="img/blog/large-3.jpg" alt=""></a>
+                                <div class="date-time">
+                                    <span class="date">03</span>
+                                    <span class="month">Jan</span>
+                                </div>
+                            </div>
+                            <div class="single-blog-post-text">
+                                <h4><a href="blog-details.html">Lorem Ipsum is simply dummy text of printing.....</a></h4>
+                                <div class="author-comments">
+                                    <span><i class="fa fa-user"></i>KATHIE BLOSSOM</span>
+                                    <span><i class="fa fa-comment"></i>15 Comments</span>
+                                </div>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
+                            </div>
+                            <div class="blog-button-links">
+                                <a href="blog-details.html" class="blog-button">Learn More</a>
+                                <div class="blog-links">
+                                    <a href="#"><i class="fa fa-facebook"></i></a>
+                                    <a href="#"><i class="fa fa-twitter"></i></a>
+                                    <a href="#"><i class="fa fa-google-plus"></i></a>
+                                    <a href="#"><i class="fa fa-linkedin"></i></a>
+                                    <a href="#"><i class="fa fa-rss"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="single-blog-post">
+                            <div class="single-blog-post-img">
+                                <a href="#"><img src="img/blog/large-4.jpg" alt=""></a>
+                                <div class="date-time">
+                                    <span class="date">30</span>
+                                    <span class="month">Jul</span>
+                                </div>
+                            </div>
+                            <div class="single-blog-post-text">
+                                <h4><a href="blog-details.html">Lorem Ipsum has been the industry's of.....</a></h4>
+                                <div class="author-comments">
+                                    <span><i class="fa fa-user"></i>ALICE HINES</span>
+                                    <span><i class="fa fa-comment"></i>15 Comments</span>
+                                </div>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
+                            </div>
+                            <div class="blog-button-links">
+                                <a href="blog-details.html" class="blog-button">Learn More</a>
+                                <div class="blog-links">
+                                    <a href="#"><i class="fa fa-facebook"></i></a>
+                                    <a href="#"><i class="fa fa-twitter"></i></a>
+                                    <a href="#"><i class="fa fa-google-plus"></i></a>
+                                    <a href="#"><i class="fa fa-linkedin"></i></a>
+                                    <a href="#"><i class="fa fa-rss"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="clearfix"></div>
+                        <div class="pagination-content">
+                            <div class="pagination-button">
+                                <ul class="pagination">
+                                    <li><a href="#"><i class="fa fa-angle-left"></i></a></li>
+                                    <li class="current"><a href="#">1</a></li>
+                                    <li><a href="#">2</a></li>
+                                    <li><a href="#">3</a></li>
+                                    <li><a href="#">4</a></li>
+                                    <li><a href="#">5</a></li>
+                                    <li><a href="#"><i class="fa fa-angle-right"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>       
+                </div>
+            </div>
+        </div>
+        <!--End of Blog Post Area -->
+
+<?php get_footer(); ?>
