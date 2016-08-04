3d1c0f121a311f4f6011fe76f14e6642fae5878d
pierab <pierinacevedob@gmail.com>
2016-08-04 16:44:18 -0500

    maldito filezila

--- single-blog.php
+++ single-blog.php
@@ -1,4 +1,4 @@
-
+<?php get_header(); ?>
         <!--Banner Area Start-->
         <div class="banner-area blog-one">
             <div class="container">
@@ -27,120 +27,7 @@
         <div class="blog-post-area section-padding">
             <div class="container">
                 <div class="row">
-                    <div class="col-md-3">
-                        <div class="sidebar-widget">
-                            <div class="single-sidebar-widget">
-                                <h4>Search <span>Blog</span></h4>
-                                <form id="text-search" action="#">
-                                    <input type="text" placeholder="Search Here .....">
-                                    <button class="submit"><i class="fa fa-search"></i></button>
-                                </form>
-                            </div>
-                            <div class="clearfix"></div>
-                            <div class="single-sidebar-widget country-select">
-                                <h4>Select <span>Categories</span></h4>
-                                <ul class="widget-categories">
-                                    <li><a href="#">Hiking <span>(10)</span></a></li>
-                                    <li><a href="#">Camping <span>(175)</span></a></li>
-                                    <li><a href="#">Trekking <span>(25)</span></a></li>
-                                    <li><a href="#">Safari <span>(18)</span></a></li>
-                                    <li><a href="#">Polar <span>(247)</span></a></li>
-                                    <li><a href="#">Mountain <span>(95)</span></a></li>
-                                    <li><a href="#">Biking <span>(39)</span></a></li>
-                                    <li><a href="#">Climbing <span>(69)</span></a></li>
-                                    <li class="no-margin"><a href="#" class="no-margin">Sailing <span>(27)</span></a></li>  
-                                </ul>
-                            </div>
-                            <div class="single-sidebar-widget">
-                                <h4>Recent <span>Posts</span></h4>
-                                <div class="single-widget-posts">
-                                    <div class="post-img">
-                                        <a href="#"><img src="img/blog/3.jpg" alt=""></a>
-                                    </div>
-                                    <div class="posts-text">
-                                        <h4><a href="#">Himalaia Trip | Europe</a></h4>
-                                        <p><i class="fa fa-clock-o"></i> May 27, 2015</p>
-                                    </div>
-                                </div>
-                                <div class="single-widget-posts">
-                                    <div class="post-img">
-                                        <a href="#"><img src="img/blog/4.jpg" alt=""></a>
-                                    </div>
-                                    <div class="posts-text">
-                                        <h4><a href="#">Himalaia Trip | Nepal</a></h4>
-                                        <p><i class="fa fa-clock-o"></i> Aug 09, 2016</p>
-                                    </div>
-                                </div>
-                                <div class="single-widget-posts no-margin">
-                                    <div class="post-img">
-                                        <a href="#"><img src="img/blog/5.jpg" alt=""></a>
-                                    </div>
-                                    <div class="posts-text">
-                                        <h4><a href="#">Himalaia Trip | China</a></h4>
-                                        <p><i class="fa fa-clock-o"></i> Jun 22, 2016</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="single-sidebar-widget">
-                                <h4>Blog <span>Archives</span></h4>
-                                <div class="blog-archive">
-                                    <select class="archive" name="archive">
-                                        <option>Select Month</option>
-                                        <option>January</option>
-                                        <option>February</option>
-                                        <option>March</option>
-                                        <option>April</option>
-                                        <option>May</option>
-                                        <option>June</option>
-                                        <option>July</option>
-                                        <option>August</option>
-                                        <option>September</option>
-                                        <option>October</option>
-                                        <option>November</option>
-                                        <option>December</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="single-sidebar-widget icon-bottom tooltip-icons">
-                                <h4>Blog <span>Tags</span></h4>
-                                <div class="widget-icon">
-                                    <span><a href="#" data-toggle="tooltip" title="Tents"><img alt="" src="img/icon/25.png"></a></span>
-                                    <span><a href="#" data-toggle="tooltip" title="Hiking"><img alt="" src="img/icon/26.png"></a></span>
-                                    <span><a href="#" data-toggle="tooltip" title="Cycling"><img alt="" src="img/icon/27.png"></a></span>
-                                    <span><a href="#" data-toggle="tooltip" title="Beach"><img alt="" src="img/icon/28.png"></a></span>
-                                    <span class="no-margin"><a href="#" data-toggle="tooltip" title="Ship Tour"><img alt="" src="img/icon/29.png"></a></span>
-                                    <span class="no-margin"><a href="#" data-toggle="tooltip" title="Boat Tour"><img alt="" src="img/icon/30.png"></a></span>
-                                    <span class="no-margin"><a href="#" data-toggle="tooltip" title="Water Games"><img alt="" src="img/icon/31.png"></a></span>
-                                    <span class="no-margin"><a href="#" data-toggle="tooltip" title="Jungle"><img alt="" src="img/icon/32.png"></a></span>
-                                </div>
-                            </div>
-                            <div class="clearfix"></div>
-                            <div class="single-sidebar-widget widget-gallery">
-                                <h4>Photo <span>Gallery</span></h4>
-                                <div class="row">
-                                    <div class="col-md-4 col-sm-2 col-xs-4">
-                                        <a href="#"><img src="img/blog/6.jpg" alt=""></a>
-                                    </div>
-                                    <div class="col-md-4 col-sm-2 col-xs-4">
-                                        <a href="#"><img src="img/blog/7.jpg" alt=""></a>
-                                    </div>
-                                    <div class="col-md-4 col-sm-2 col-xs-4">
-                                        <a href="#"><img src="img/blog/8.jpg" alt=""></a>
-                                    </div>
-                                    <div class="col-md-4 col-sm-2 col-xs-4">
-                                        <a href="#"><img src="img/blog/9.jpg" alt=""></a>
-                                    </div>
-                                    <div class="col-md-4 col-sm-2 col-xs-4">
-                                        <a href="#"><img src="img/blog/10.jpg" alt=""></a>
-                                    </div>
-                                    <div class="col-md-4 col-sm-2 col-xs-4">
-                                        <a href="#"><img src="img/blog/11.jpg" alt=""></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-9">
+                    <div class="col-md-12">
                         <div class="single-blog-post blog-post-details">
                             <div class="single-blog-post-img">
                                 <a href="#"><img src="img/blog/large-1.jpg" alt=""></a>
@@ -239,4 +126,4 @@
             </div>
         </div>
         <!--End of Blog Post Area -->
-       
+        <?php get_footer(); ?>
