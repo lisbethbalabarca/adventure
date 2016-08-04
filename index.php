f2bb28d113a55c1eee1eecfd4f374282aca3987b
lisbethbalabarca <lisbethbalabarca@gmail.com>
2016-08-04 10:39:13 -0500

    ahora con los iconos

--- index.php
+++ index.php
@@ -111,13 +111,16 @@
                 <?php }
             } ?>
         </div>
-
-
     </div>
 </div>
 </div>
 <!--End of Portfolio Area-->
 <!--Fun Factor Area Start-->
+
+
+<?php if (have_rows('activity', 'options')) { ?>
+
+
 <div class="fun-factor-area fun-factor-two text-center hidden-xs">
     <div class="container">                
         <div class="row">
@@ -131,80 +134,29 @@
         </div>
         <div class="row">
             <div class="col-md-2 col-sm-3">
+                <?php 
+                    while (have_rows('activity', 'options')) { the_row();
+                ?>
                 <div class="single-fun-wrapper">
                     <div class="single-fun-factor">
                         <div class="fun-border-left"></div>
-                        <img src="<?php bloginfo('template_url') ?>/img/icon/7.png" class="primary-img" alt="">
+                        <?php 
+                            $activity= get_sub_field('image');
+                            $image= wp_get_attachment_image_src($activity, 'activity');
+                        ?>  
+                        <img src="<?php echo $image[0]; ?>" class="primary-img" alt="">
                         <img src="<?php bloginfo('template_url') ?>/img/icon/7-hover.png" class="secondary-img" alt="">
-                        <h4><span class="counter">5670</span> M</h4>
-                        <h5>Cyliing Track</h5>
+                        <h4><span class="counter"><?php the_sub_field('score') ?></span> M</h4>
+                        <h5><?php the_sub_field('title') ?></h5>
                         <div class="fun-border-right"></div>
                     </div>
-                </div>    
-            </div>
-            <div class="col-md-2 col-sm-3">
-                <div class="single-fun-wrapper">
-                    <div class="single-fun-factor">
-                        <div class="fun-border-left"></div>
-                        <img src="<?php bloginfo('template_url') ?>/img/icon/8.png" class="primary-img" alt="">
-                        <img src="<?php bloginfo('template_url') ?>/img/icon/8-hover.png" class="secondary-img" alt="">
-                        <h4><span class="counter">480</span> M</h4>
-                        <h5>Distance Coverd</h5>
-                        <div class="fun-border-right"></div>
-                    </div>
-                </div>    
-            </div>
-            <div class="col-md-2 col-sm-3">
-                <div class="single-fun-wrapper">
-                    <div class="single-fun-factor">
-                        <div class="fun-border-left"></div>
-                        <img src="<?php bloginfo('template_url') ?>/img/icon/9.png" class="primary-img" alt="">
-                        <img src="<?php bloginfo('template_url') ?>/img/icon/9-hover.png" class="secondary-img" alt="">
-                        <h4><span class="counter">5670</span> M</h4>
-                        <h5>Altitude Hiked</h5>
-                        <div class="fun-border-right"></div>
-                    </div>
-                </div>    
-            </div>
-            <div class="col-md-2 col-sm-3">
-                <div class="single-fun-wrapper">
-                    <div class="single-fun-factor">
-                        <div class="fun-border-left"></div>
-                        <img src="<?php bloginfo('template_url') ?>/img/icon/10.png" class="primary-img" alt="">
-                        <img src="<?php bloginfo('template_url') ?>/img/icon/10-hover.png" class="secondary-img" alt="">
-                        <h4><span class="counter">180</span> KM</h4>
-                        <h5>Sailing Distance</h5>
-                        <div class="fun-border-right"></div>
-                    </div>
-                </div>    
-            </div>
-            <div class="col-md-2 hidden-sm">
-                <div class="single-fun-wrapper">
-                    <div class="single-fun-factor">
-                        <div class="fun-border-left"></div>
-                        <img src="<?php bloginfo('template_url') ?>/img/icon/11.png" class="primary-img" alt="">
-                        <img src="<?php bloginfo('template_url') ?>/img/icon/11-hover.png" class="secondary-img" alt="">
-                        <h4><span class="counter">570</span> Days</h4>
-                        <h5>Spent Adventure</h5>
-                        <div class="fun-border-right"></div>
-                    </div>
-                </div>    
-            </div>
-            <div class="col-md-2 hidden-sm">
-                <div class="single-fun-wrapper">
-                    <div class="single-fun-factor">
-                        <div class="fun-border-left"></div>
-                        <img src="<?php bloginfo('template_url') ?>/img/icon/12.png" class="primary-img" alt="">
-                        <img src="<?php bloginfo('template_url') ?>/img/icon/12-hover.png" class="secondary-img" alt="">
-                        <h4><span class="counter">567</span> M</h4>
-                        <h5>Jugle Distance</h5>
-                        <div class="fun-border-right"></div>
-                    </div>
-                </div>    
+                </div>
+                <?php } ?>   
             </div>
         </div>
     </div>
 </div>
+<?php } ?>
 <!--End of Fun Factor Area-->
 <!--Blog Area Start-->
 <div class="blog-area section-padding blog-two-area">
