4998119cc53008bb2770adc95ac35838bf8cb3e4
lisbethbalabarca <lisbethbalabarca@gmail.com>
2016-08-08 18:26:12 -0500

    cambios everywheregit add .

--- js/main.js
+++ js/main.js
@@ -84,15 +84,3 @@
 -------------------------------------- */
     $('[data-toggle="tooltip"]').tooltip(); 
 })(jQuery); 
-
-/*------------------------------------
-    map
--------------------------------------- */
-$(document).ready(function() {
-   var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(-34.397, 150.644),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-}
-var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-}
