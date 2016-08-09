b432a9e59b1d1e368d85a4a1ed59b204da784b92
pierab <pierinacevedob@gmail.com>
2016-08-09 17:36:36 -0500

    de todo un poco

--- footer.php
+++ footer.php
@@ -37,21 +37,10 @@
         <div class="footer-area footer-two">
             <div class="container">
                 <div class="row">
-                    <div class="col-lg-4 col-md-5 col-sm-12">
+                    <div class="col-lg-12 col-md-12 col-sm-12 text-center">
                         <span>Copyright © 2016 <a href="#">Power-Boosts</a>. All rights reserved.</span>
                     </div>
-                    <div class="col-lg-8 col-md-7 col-sm-12">
-                        <nav>
-                            <ul id="footer-menu">
-                                <li><a href="index.html">Home</a></li>
-                                <li><a href="shop-grid-no-sidebar.html">Destinations</a></li>
-                                <li><a href="shop-grid-no-sidebar.html">Travel Styles</a></li>
-                                <li><a href="shop-grid-no-sidebar.html">Specials</a></li>
-                                <li><a href="blog-1.html">Blog</a></li>
-                                <li><a href="contact.html">Contact Us</a></li>
-                            </ul>
-                        </nav>
-                    </div>
+                   
                 </div>
             </div>
         </div>
